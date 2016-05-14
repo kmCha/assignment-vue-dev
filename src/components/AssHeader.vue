011f700189d6d908a9c85d365e4b129e1ceca24d
kM <chamajiuxi@126.com>
2016-05-14 10:39:35 -0400

    header nav styled

--- src/components/AssHeader.vue
+++ src/components/AssHeader.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="header">
-    <ul class="list list-left">
-      <li>logo</li>
-      <li>登录</li>
-      <li>注册</li>
+    <ul class="list">
+      <li class="item">Assignment Recorder</li>
+      <li class="item item-left">登录</li>
+      <li class="item item-left">注册</li>
+      <li class="item item-right">
+        <img class="nav-logo" src="../assets/logo.png" alt="logo" />
+      </li>
     </ul>
   </div>
 </template>
@@ -27,9 +30,26 @@
     list-style: none;
     margin-top: 0;
     padding: 0 1em;
-    li {
+    overflow: hidden;
+
+    .item {
       display: inline-block;
+      font-size: 0.88em;
       padding: 0.5em 0.3em;
+      cursor: pointer;
+      transition: background-color 0.3s;
+    }
+    .item-left {
+      &:hover {
+        background-color: lighten(@grey, 10%);
+      }
+    }
+    .item-right {
+      float: right;
+    }
+    .nav-logo {
+      height: 0.88em;
+      width: 0.88em;
     }
   }
 </style>
